87c13cf19f8b59c3d0c63c16f6a814c4ab32e3f2
NahuelVilla <NahuelVilla@users.noreply.github.com>
2025-04-05 02:25:36 +0000

    deploy: 1191873e224ef420336b249f95474ffa7ba5ab23

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Sobre mí",
+        "excerpt":"¡Hola! Soy Nahuel Villa, soon I will be describing here my professional path.   Descargar CV  ","categories": [],
+        "tags": [],
+        "url": "/about/",
+        "teaser": null
+      },{
+        "title": "Proyectos",
+        "excerpt":"ARU Robot v1.5  Logro: Reducción del 40% en errores de estabilización en terrenos irregulares.  Tecnologías: ROS, C++, Simulink.  ","categories": [],
+        "tags": [],
+        "url": "/projects/",
+        "teaser": null
+      },{
+        "title": "Place holder for a post",
+        "excerpt":"Pronto compartiré artículos técnicos sobre robótica y algoritmos de control.  ","categories": [],
+        "tags": [],
+        "url": "/2024/05/20/bienvenida.html",
+        "teaser": null
+      }]
